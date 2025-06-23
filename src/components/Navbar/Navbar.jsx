5a6e35f27f7284193af34e48199a236966058f8d
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-23 21:45:43 +0600

    bug fixed in navbar route and added a form in singup file

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
    <li><NavLink to="/">Home</NavLink></li>
    <li><NavLink to="/login">Login</NavLink></li>
    <li><NavLink to="/register">Register</NavLink></li>
-   <li><NavLink to='/signUp'>SingUp</NavLink></li>
+   <li><NavLink to='/signUp'>SignUp</NavLink></li>
 
  </>
     return (
