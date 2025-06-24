d0bcf7d4d662a02b43210a9504ac71af465b3f65
ahassh1 <ahasanhabibpolash420@gmail.com>
2025-06-24 19:13:57 +0600

    error message done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,26 +1,66 @@
-import React from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
 import { Link } from "react-router";
+import { auth } from "../../firebase.init";
 
 const Login = () => {
-    const handleLogin =() =>{
-        e.preventDefault();
-    }
+  const [errorMessage, setErrorMessage] = useState("");
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    const email =e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+
+    //reset 
+    setErrorMessage('');
+
+    // login user
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+        setErrorMessage(error.message)
+      });
+  };
+
+
   return (
     <div className="card mt-8 bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
         <h1 className="text-5xl font-bold">Login now!</h1>
         <form onClick={handleLogin} className="fieldset">
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input
+            type="email"
+            className="input"
+            name="email"
+            placeholder="Email"
+          />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input
+            type="password"
+            className="input"
+            name="password"
+            placeholder="Password"
+          />
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
           <button className="btn btn-neutral mt-4">Login</button>
         </form>
 
-        <p>New to the website , Please <Link className="text-blue-400 underline" to='/singUp'>Sing Up</Link></p>
+        <p>
+          New to the website , Please{" "}
+          <Link className="text-blue-400 underline" to="/singUp">
+            Sing Up
+          </Link>
+        </p>
+        {
+            errorMessage && <p className="text-red-500">{errorMessage}</p>
+        }
       </div>
     </div>
   );
